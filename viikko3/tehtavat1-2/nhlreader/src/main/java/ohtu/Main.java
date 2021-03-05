18feec82e75aa722a89fa7f9785819d4213da2f3
Mika R <63600879+Pentu88@users.noreply.github.com>
2021-03-05 20:14:56 +0200

    Viikko 3 - Tehtävä 2

--- viikko3/tehtavat1-2/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/tehtavat1-2/nhlreader/src/main/java/ohtu/Main.java
@@ -2,6 +2,8 @@ package ohtu;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.Arrays;
+
 import org.apache.http.client.fluent.Request;
 
 public class Main {
@@ -10,16 +12,23 @@ public class Main {
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
+        // System.out.println("json-muotoinen data:");
+        // System.out.println( bodyText );
 
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
 
+        Arrays.sort(players);
         System.out.println("Oliot:");
         for (Player player : players) {
-            System.out.println(player);
+            if (player.getNationality().equals("FIN")) {
+                System.out.println(player);
+            }
         }
+
+        // System.out.print("(" + players[0].getNationality().getClass().getSimpleName() + ") " + players[0].getNationality() + " - " );
+        // System.out.println(players[0].getNationality().equals("CAN"));
+        // System.out.println("Pisin pelaajan nimi = " + maxName); // 25
     }
 
 }
--- viikko3/tehtavat1-2/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/tehtavat1-2/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,12 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player> {
     private String name;
+    private String nationality;
+    private String team;
+    private int goals;
+    private int assists;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +16,55 @@ public class Player {
         return name;
     }
 
+    // Lisätään ominaisuuksia
+    public void setNationality(String nationality){ this.nationality = nationality;}
+
+    public String getNationality(){ return this.nationality;}
+
+    public void setTeam(String team){ this.team = team;}
+
+    public String getTeam(){ return this.team;}
+
+    public void setGoals(int goals){ this.goals = goals;}
+
+    public int getGoals(){ return this.goals;}
+
+    public void setAssists(int assists){ this.assists = assists;}
+
+    public int getAssists(){ return this.assists;}
+
+    public  int getPoints() { return  this.goals + this.assists;}
+    //
+
     @Override
     public String toString() {
-        return name;
+        return this.name2s() + team + stats2s(goals) + "+" + stats2s(assists) + "=" + stats2s(this.getPoints());
     }
-      
+
+    // This is so ugly
+    private String name2s() {
+        int n = 25 - name.length();
+        String output = name;
+
+        for (int i = 0; i < n; i++) {
+            output += " ";
+        }
+        return output;
+    }
+
+    // This is so ugly
+    private String stats2s(int nro) {
+        String output = " ";
+        if (nro < 10) {
+            output += " ";
+        } /* else if(nro < 100){
+            output += " ";
+        } */
+
+        return output + nro + " ";
+    }
+
+    public int compareTo(Player p) {
+        return p.getPoints() - this.getPoints();
+    }
 }
