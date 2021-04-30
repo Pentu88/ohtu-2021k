2b017f122a5d19d50b32057822518a160876444a
Mika R <63600879+Pentu88@users.noreply.github.com>
2021-04-30 13:17:17 +0300

    UPDATE Paaohjelma.java

--- viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
+++ viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
@@ -3,8 +3,5 @@ package ohtu.kivipaperisakset;
 import java.util.Scanner;
 
 public class Paaohjelma {
-    private static final Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        KiviPaperiSaksetPeli.suoritaKPSPeli();
-    }
+    public static void main(String[] args) { KiviPaperiSaksetPeli.suoritaKPSPeli(); }
 }
